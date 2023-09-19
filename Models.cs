cef8cbf815832a5773764bc6ebd0bece909679f2
Wesley254 <wesleykirui2021@gmail.com>
2023-09-19 16:09:38 +0300

    first commit

---
+++ Models.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShadowProperties
+{
+    // Define the User class, representing a user entity in the database.
+    public class User
+    {
+        public int Id { get; set; } // Unique identifier for the user.
+        public string Name { get; set; } = string.Empty; // User's name.
+
+        // Navigation property representing a collection of associated Test entities.
+        public ICollection<Test> Tests { get; set; } = new List<Test>();
+    }
+
+    // Define the Test class, representing a test entity in the database.
+    public class Test
+    {
+        public int Id { get; set; } // Unique identifier for the test.
+        public string Text { get; set; } = string.Empty; // Text associated with the test.
+    }
+
+    // Define the DbContext class named "Db" for interacting with the database.
+    public class Db : DbContext
+    {
+        // Configure the database connection options when the DbContext is created.
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Specify that the DbContext should use SQL Server as the database provider
+            // and provide the connection string.
+            optionsBuilder.UseSqlServer("# YOUR CONNECTION STRING HERE #");
+        }
+
+        // Define the database model by configuring entities and their relationships.
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configure the relationship between Test and User entities.
+            modelBuilder.Entity<Test>()
+                .HasOne<User>()
+                .WithMany(x => x.Tests)
+                .HasForeignKey("UserId") // Define a foreign key named "UserId."
+                .IsRequired(); // Specify that the foreign key is required.
+
+            // Define a shadow property "LastModified" for the Test entity.
+            modelBuilder.Entity<Test>()
+                .Property<DateTime>("LastModified");
+        }
+
+        // Define DbSet properties for User and Test entities, allowing database queries.
+        public DbSet<User> Users { get; set; } = null!; // DbSet for User entities.
+        public DbSet<Test> Tests { get; set; } = null!; // DbSet for Test entities.
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShadowProperties;
+
+using var db = new Db(); // Creating a new instance of the DbContext named 'db'
+await db.Database.EnsureCreatedAsync(); // Ensuring that the database schema is created (if it doesn't exist)
+
+User user = new() // Creating a new User object and initializing it using object initializer syntax
+{
+    Name = "Wesley", // Setting the 'Name' property of the User object
+    Tests = new Test[] // Initializing the 'Tests' property as an array of Test objects
+    {
+        new() { Text = "Understanding Shadow Properties" } // Creating a new Test object with a 'Text' property
+    }
+};
+
+db.Users.Add(user); // Adding the User object to the 'Users' DbSet in the DbContext
+
+await db.SaveChangesAsync(); // Saving changes to the database
+
+var test = await db.Tests.FirstAsync(); // Retrieving the first Test object asynchronously from the 'Tests' DbSet
+
+var userId = db.Entry(test).Property<int>("UserId").CurrentValue; // Getting the 'UserId' property value of the 'test' object using shadow property
+
+Console.WriteLine($"user: {userId} tested: \"{test.Text}\""); // Printing the user and test information to the console
+
+var tests = db.Tests.Where(x => EF.Property<int>(x, "UserId") == 1).ToArray();
+// Querying the 'Tests' DbSet to find Test objects where the 'UserId' shadow property is equal to 1 and converting the result to an array
+
+Console.WriteLine(tests[0].Text); // Printing the 'Text' property of the first Test object in the 'tests' array to the console
